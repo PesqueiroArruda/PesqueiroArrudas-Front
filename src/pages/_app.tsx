258d3bca7b20a85069f8e10509950895dcfd0bc2
Diego Arruda <arrudadiego45@gmail.com>
2022-07-20 19:26:07 -0300

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,8 @@ interface SocketProps {
 export const SocketContext = createContext({} as SocketProps);
 
 // const devUrl = 'http://localhost:8080';
-const prodUrl = process.env.API_URL as string;
+// const prodUrl = process.env.API_URL as string;
+const prodUrl = "https://pesqueiroarruda-back.herokuapp.com"
 
 const socket = io(prodUrl);
 
--- src/services/serverApi.ts
+++ src/services/serverApi.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const devUrl = 'http://localhost:8080';
-const prodUrl = process.env.API_URL;
+// const prodUrl = process.env.API_URL;
+const prodUrl = "https://pesqueiroarruda-back.herokuapp.com"
 
 export const serverApi = axios.create({
   baseURL: prodUrl,
